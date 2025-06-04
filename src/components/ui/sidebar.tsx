d1007e473105a571de3ce08e3b8a3855eef9c785
CDC LĐK <ldkp.cdc@gmail.com>
2025-06-04 03:43:26 +0000

    Ứng dụng chuyển trang chưa mượt, bỏ dấu liệt kê (dấu chấm) trước tên mỗi module trên sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -492,7 +493,7 @@ const SidebarMenu = React.forwardRef<
   <ul
     ref={ref}
     data-sidebar="menu"
-    className={cn("flex w-full min-w-0 flex-col gap-1", className)}
+    className={cn("flex w-full min-w-0 flex-col gap-1 list-none", className)}
     {...props}
   />
 ))
@@ -690,7 +691,7 @@ const SidebarMenuSub = React.forwardRef<
     ref={ref}
     data-sidebar="menu-sub"
     className={cn(
-      "mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l border-sidebar-border px-2.5 py-0.5",
+      "mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l border-sidebar-border px-2.5 py-0.5 list-none",
       "group-data-[collapsible=icon]:hidden",
       className
     )}
